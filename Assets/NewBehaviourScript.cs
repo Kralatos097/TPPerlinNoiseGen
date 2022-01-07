76ace914b73183cd15a9294105ab744dd6cbd869
Kralatos <thibaud097@gmail.com>
2022-01-07 11:47:41 +0100

    push fin

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -11,7 +11,7 @@ public class NewBehaviourScript : MonoBehaviour
     public int xSize;
     public int ySize;
 
-    [Range(0.01f,1)]
+    [Range(0.01f,50)]
     public float zoom;
 
     public float xOffset;
@@ -19,6 +19,9 @@ public class NewBehaviourScript : MonoBehaviour
 
     [Range(0,1)]
     public float floorValue = 0.6f;
+    
+    [Range(0,1)]
+    public float floorValueB = 0.6f;
 
     public Tilemap Tilemap;
     public Tilemap OverTilemap;
@@ -30,50 +33,64 @@ public class NewBehaviourScript : MonoBehaviour
 
     private float xSeed;
     private float ySeed;
+    private float xSeedB;
+    private float ySeedB;
+
+    private int[,] grid;
 
     // Start is called before the first frame update
     void Update()
     {
+        Tilemap.ClearAllTiles();
+        OverTilemap.ClearAllTiles();
+        grid = new int[xSize,ySize];
         System.Random rnd = new System.Random(seed);
 
-        xSeed = (float)rnd.NextDouble();
-        ySeed = (float)rnd.NextDouble();
+        xSeed = rnd.Next(50)+(float)rnd.NextDouble();
+        ySeed = rnd.Next(50)+(float)rnd.NextDouble();
+        
+        xSeedB = rnd.Next(50)+(float)rnd.NextDouble();
+        ySeedB = rnd.Next(50)+(float)rnd.NextDouble();
 
         for (int y = 0; y < ySize; y++)
         {
             for (int x = 0; x < xSize; x++)
             {
-                float xCoord = (xSeed + x) * zoom + xOffset;
-                float yCoord = (ySeed + y) * zoom + yOffset;
+                float xCoord = (xSeed + x) / zoom + xOffset;
+                float yCoord = (ySeed + y) / zoom + yOffset;
                 
                 float pNValue = Mathf.PerlinNoise(xCoord, yCoord);
+                
+                float xCoordB = (xSeedB + x) / zoom + xOffset;
+                float yCoordB = (ySeedB + y) / zoom + yOffset;
+                
+                float pNValueBis = Mathf.PerlinNoise(xCoordB, yCoordB);
 
-                //Debug.Log(pNValue + " / " + xSeed+x + " / " + ySeed+y);
+                //Debug.Log(pNValue + " / " + pNValueBis);
                 
                 Vector3Int pos = new Vector3Int(x, y,0);
                 if(pNValue > floorValue)
                 {
-                    if (pNValue > 0.85f)
-                    {
-                        OverTilemap.SetTile(pos, LianeTile);
-                    }
-                    else
-                    {
-                        OverTilemap.SetTile(pos, null);
-                    }
                     Tilemap.SetTile(pos, blackTile);
+                    grid[x, y] = 1;
                 }
                 else
                 {
-                    if(pNValue < 0.1f)
-                    {
-                        OverTilemap.SetTile(pos, GreenTile);
-                    }
-                    else
-                    {
-                        OverTilemap.SetTile(pos, null);
-                    }
                     Tilemap.SetTile(pos, whiteTile);
+                    grid[x, y] = 0;
+                }
+
+                if (pNValueBis < 1-floorValueB && grid[x,y] == 1)
+                {
+                    OverTilemap.SetTile(pos, LianeTile);
+                }
+                else if (pNValueBis > floorValueB && grid[x,y] == 0)
+                {
+                    OverTilemap.SetTile(pos, GreenTile);
+                }
+                else
+                {
+                    OverTilemap.SetTile(pos, null);
                 }
                 
             }
